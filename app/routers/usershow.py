eacefe6ab545a32c257dec6abcd7071e7c282fe5
Wendy <kutopusu@gmail.com>
2024-11-12 16:52:24 -0300

    update user show routes

--- app/routers/usershow.py
+++ app/routers/usershow.py
@@ -6,24 +6,16 @@ from app.auth import verify_access_token
 
 router = APIRouter()
 
-@router.put("/usershow/{show_id}/going", response_model=schemas.UserShow)
-def marked_as_going(show_id: int, db: Session = Depends(get_db), token: str = Depends(verify_access_token)):
+@router.post("/shows/{show_id}/going", response_model=schemas.UserShow)
+def mark_as_going(show_id: int, token: str = Depends(verify_access_token), db: Session = Depends(get_db)):
     user_id = token.get("user_id")
-    if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not authenticated")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    return crud.mark_show_as_going(db=db, user_id=user_id, show_id=show_id)
 
-    usershow = crud.update_marked_as_going(db, user_id=user_id, show_id=show_id)
-    if usershow is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="UserShow record not found")
-    return usershow
-
-@router.put("/usershow/{show_id}/attended", response_model=schemas.UserShow)
-def marked_as_attended(show_id: int, db: Session = Depends(get_db), token: str = Depends(verify_access_token)):
+@router.post("/shows/{show_id}/attended", response_model=schemas.UserShow)
+def mark_as_attended(show_id: int, token: str = Depends(verify_access_token), db: Session = Depends(get_db)):
     user_id = token.get("user_id")
-    if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not authenticated")
-
-    usershow = crud.update_marked_as_attended(db, user_id=user_id, show_id=show_id)
-    if usershow is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="UserShow record not found")
-    return usershow
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    return crud.mark_show_as_attended(db=db, user_id=user_id, show_id=show_id)
