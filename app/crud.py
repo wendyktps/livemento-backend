269a88fe54e941b770b8d4da216676947383b252
Wendy <86799382+wendyktps@users.noreply.github.com>
2024-11-19 17:39:13 -0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -48,9 +48,15 @@ def get_user(db: Session, user_id: int):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email já está em uso. Tente outro.",
+        )
+    
     hashed_password = bcrypt.hash(user.password)
-    db_user = models.User(email=user.email, name=user.name,
-                          password_hash=hashed_password)
+    db_user = models.User(email=user.email, name=user.name, password_hash=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
